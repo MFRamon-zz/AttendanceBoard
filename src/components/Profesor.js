4c10b2c338737d8ef5aff1f0ebdbf75356b5ac7b
Ramon Manrique <ramon.manfig@gmail.com>
2019-10-27 09:43:05 -0600

    Updated Labels with English Text

--- src/components/Profesor.js
+++ src/components/Profesor.js
@@ -1,13 +1,13 @@
 import React, {Fragment} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+//import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
+//import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
+//import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Avatar from '@material-ui/core/Avatar';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCheckSquare, faEye } from "@fortawesome/free-solid-svg-icons";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
 
 const Profesor = () => {
     return (
