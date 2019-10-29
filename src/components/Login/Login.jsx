40e688585d48378594345f2287d11f2d5e617080
Ramon Manrique <ramon.manfig@gmail.com>
2019-10-28 18:05:52 -0600

    Added necessary import for testing changes

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Sidebar from "../Sidebar/Sidebar";
+import Appbar from "../Appbar/Appbar";
 
 export class Login extends Component {
     render() {
