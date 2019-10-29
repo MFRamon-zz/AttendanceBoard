986a712bdce41594327a786019a6aaf2c772e9ca
Alan <alan9805@gmail.com>
2019-10-29 15:03:31 -0600

    Routing corrected
    Routing corrected

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from "react";
-import Main from './pages/Main'
+import React, { Component, Fragment } from 'react';
+import {BrowserRouter as Router, Route ,Switch, Redirect} from 'react-router-dom';
+import Dashboard from './pages/Dashboard/Dashboard'
 
-function App() {
-  return (
-    <div>
-      <Main/>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+
+        <Router>
+          <Switch>
+            <Route path="/" component={Dashboard}/>
+          </Switch>
+        </Router>
+
+      </Fragment>
+    );
+  }
 }
 
 export default App;
